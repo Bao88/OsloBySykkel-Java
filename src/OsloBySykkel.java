ae3c097614314e314d6e3add0371df75a863e1b0
HBN <ngohoangbao@outlook.com>
2018-03-15 19:37:06 +0100

    Resource successfully requested

--- src/OsloBySykkel.java
+++ src/OsloBySykkel.java
@@ -1,23 +1,32 @@
 import java.net.*;
 import java.io.*;
+import org.json.*;
+import java.util.*;
 
 public class OsloBySykkel {
 
-	public static void main(String[] args) throws Exception {
+	private static JSONArray requestData(String resource) throws Exception {
+		URL url = new URL(resource);
+		URLConnection connect = url.openConnection();;
+		connect.setRequestProperty("Client-Identifier", "aa57ec526474715c30fdc77f2a7da0c0");
 		
-		System.out.println("Hello World");
-		URL stations = new URL("https://oslobysykkel.no/api/v1/stations");
-		URL availabity = new URL("https://oslobysykkel.no/api/v1/stations/availability");
-		
-		URLConnection cStations = stations.openConnection();
-		cStations.setRequestProperty("Client-Identifier", "aa57ec526474715c30fdc77f2a7da0c0");
-		System.out.println(cStations.getHeaderFields());
-		BufferedReader in = new BufferedReader(new InputStreamReader(cStations.getInputStream()));
-		String inputLine;
-		while((inputLine = in.readLine()) != null) {
-			System.out.println(inputLine);
-		}
+		BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+		JSONArray json = (new JSONObject(in.readLine())).getJSONArray("stations");
 		in.close();
+		return json;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		//System.out.println("Hello World");
+		//URL stationsURL = new URL("https://oslobysykkel.no/api/v1/stations");
+		//URL availabilityURL = new URL("https://oslobysykkel.no/api/v1/stations/availability");
+		
+		JSONArray stations = requestData("https://oslobysykkel.no/api/v1/stations");
+		JSONArray availability = requestData("https://oslobysykkel.no/api/v1/stations/availability");
+		
+		System.out.println(stations.getJSONObject(0));
+		System.out.println(availability.getJSONObject(0));
+		
 	}
 
 }
